6d220d783fffa79b8e99cbb7aee13d1431d988d4
Maciej Karpiński <karpfly@gmail.com>
2020-03-17 18:21:26 +0100

    dodałem podstawową obsługe bazy danych

--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,25 @@ namespace Projekt_PSBD
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Debug.WriteLine("naciskam button");
+            using (var ctx = new MagazynContext())
+            {
+                Debug.WriteLine("var towar");
+                var towar = new Towar()
+                {
+                    NazwaTowaru = "MateBook D 15",
+                    Producent = new Producent() { NazwaProducenta = "Huawei", AdresProducenta = "Wuhan", Kraj = new Kraj() { NazwaKraju = "Chiny" } },
+                    CenaTowaru = 2.99,
+                    IloscTowaru = 100
+                };
+                Debug.WriteLine("ctx.towary.add");
+                ctx.Towary.Add(towar);
+                ctx.SaveChanges();
+            }
         }
     }
 }
